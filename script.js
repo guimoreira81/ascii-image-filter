e9a30f323cd5897c111acd255a4c778c5526fb71
guimoreira81 <guilhermemoreiraf81@gmail.com>
2025-11-28 08:36:15 -0300

    Adding files

---
+++ script.js
@@ -0,0 +1,6 @@
+const imageElement = document.getElementById('image')
+const canvasElement = document.querySelector('canvas')
+const context = canvasElement.getContext('2d')
+
+canvasElement.width = imageElement.width
+canvasElement.height = imageElement.height
